be10b22177054aff33b0774fe00501a02b9bc2f6
yamary0 <40788982+yamary0@users.noreply.github.com>
2020-10-19 16:22:55 +0900

    arrenge whole site

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
+import Icon from '../../static/assets/profile.png'
 export default () => (
   <StaticQuery
     query={graphql`
@@ -16,12 +17,16 @@ export default () => (
     `}
     render={data => (
       <div className="hero-header">
-        <div className="headline">{data.site.siteMetadata.home.title}</div>
+        <div className="headline">
+          <img className="profile-icon" src={Icon} alt="profile icon" />
+          {data.site.siteMetadata.home.title}
+        </div>
         <div 
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+        <div className="since-date">since 2020.10.17.</div>
+        {/* <Link to='/contact' className="button -primary">Get in touch &rarr;</Link> */}
       </div>
     )}
   />
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,10 @@
 import React from "react"
-import {Link} from "gatsby"
+// import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+    {/* <Link to="/contact">Contact</Link> */}
     <ThemeChanger/>
   </nav>
   
