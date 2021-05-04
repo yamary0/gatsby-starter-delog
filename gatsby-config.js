538b74c0ba03c24d0381f9db934072981ca32b16
yamary0 <40788982+yamary0@users.noreply.github.com>
2021-05-05 01:33:28 +0900

    changed how to import GA id from netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        trackingId: "process.env.GOOGLE_ANALYTICS_ID",
         head: true,
       },
     },
